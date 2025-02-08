3b8ed0b9648e562a092fc62b1f3c90eb84cd64ca
pdaug <dev.pdaug@gmail.com>
2025-02-08 11:31:55 -0300

    add: settings page

--- src/components/source/Source.tsx
+++ src/components/source/Source.tsx
@@ -1,4 +1,3 @@
-import { tags } from "@lezer/highlight";
 import { useRef, useState } from "react";
 import ReactCodeMirror, { ReactCodeMirrorRef } from "@uiw/react-codemirror";
 
@@ -7,149 +6,27 @@ import { javascript } from "@codemirror/lang-javascript";
 import { vscodeDark, vscodeDarkInit } from "@uiw/codemirror-theme-vscode";
 
 // styles
+import {
+  SourceConfigSetup,
+  SourceConfigThemeStyles,
+  SourceConfigThemeSettings,
+} from "./SourceConfig";
 import "./Source.css";
 
-const Source = function () {
+type SourceProps = {
+  initialSourceValue?: string;
+};
+
+const Source = function ({ initialSourceValue }: SourceProps) {
   const extensions = [vscodeDark, javascript()];
   const sourceRef = useRef<ReactCodeMirrorRef | null>(null);
 
-  const red = "#ff7588";
-  const pink = "#ff8fdd";
-  const orange = "#ffa961";
-  const blue = "#84c3e0";
-  const purple = "#c2baff";
-  const green = "#d3ffa1";
-  const yellow = "#ffd86b";
+  const [sourceValue, setSourceValue] = useState(initialSourceValue || "");
 
   const theme = vscodeDarkInit({
-    settings: {
-      fontFamily: "'Roboto Mono', monospace",
-    },
-    styles: [
-      {
-        tag: tags.comment,
-        color: "#666",
-      },
-
-      {
-        tag: tags.moduleKeyword,
-        color: red,
-      },
-      {
-        tag: tags.definitionKeyword,
-        color: red,
-      },
-      {
-        tag: tags.controlKeyword,
-        color: red,
-      },
-      {
-        tag: tags.keyword,
-        color: red,
-      },
-
-      {
-        tag: tags.variableName,
-        color: blue,
-      },
-
-      {
-        tag: tags.propertyName,
-        color: purple,
-      },
-
-      {
-        tag: tags.className,
-        color: pink,
-      },
-
-      {
-        tag: tags.number,
-        color: green,
-      },
-
-      {
-        tag: tags.function(tags.variableName),
-        color: yellow,
-      },
-      {
-        tag: tags.function(tags.propertyName),
-        color: yellow,
-      },
-
-      {
-        tag: tags.string,
-        color: orange,
-      },
-      {
-        tag: tags.special(tags.string),
-        color: orange,
-      },
-    ],
+    settings: SourceConfigThemeSettings,
+    styles: SourceConfigThemeStyles,
   });
-
-  const basicSetup = {
-    foldGutter: true,
-    dropCursor: false,
-    allowMultipleSelections: false,
-    indentOnInput: false,
-  };
-
-  const [sourceValue, setSourceValue] = useState(
-    `// Example borrowed from: https://github.com/PacktPublishing/JavaScript-by-Example/blob/master/Chapter07/CompletedCode/src/services/api/apiCall.js
-// Define timeout duration
-const timeoutDuration = 12000;
-
-// Random number to better showcase syntax highlighting
-const NUMBER = 81274509182375409237;
-
-// Will make a web request to specified route
-// Returns a promise
-export function apiCall(route, body = {}, method = 'post') {
-    const request = new Promise((resolve, reject) => {
-        const headers = new Headers({
-            'Content-Type': 'application/json',
-        });
-
-        const requestDetails = {
-            method,
-            mode: 'cors',
-            headers,
-        };
-
-        if (method !== 'GET') requestDetails.body = JSON.stringify(body);
-
-        function handleErrors(response) {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw Error(response.statusText);
-            }
-        }
-
-        const serverURL = process.env.REACT_APP_SERVER_URL || \`http://localhost:3000\`;
-
-        // Make the web request w/ fetch API
-        fetch(\`\${serverURL}/\${route}\`, requestDetails)
-            .then(handleErrors)
-            .then(data => resolve(data))
-            .catch(err => reject(err));
-    });
-
-    // Define a timeout so the request cannot hang forever
-    const timeout = new Promise((_, reject) => {
-        setTimeout(reject, timeoutDuration, \`Request timed out!\`);
-    });
-
-    // Return a promise
-    return new Promise((resolve, reject) => {
-        Promise.race([request, timeout])
-            .then(result => resolve(result))
-            .catch(error => reject(error));
-    });
-}
-`,
-  );
 
   const OnChangeSource = function (value: string) {
     setSourceValue(value);
@@ -163,8 +40,8 @@ export function apiCall(route, body = {}, method = 'post') {
         ref={sourceRef}
         value={sourceValue}
         extensions={extensions}
-        basicSetup={basicSetup}
         onChange={OnChangeSource}
+        basicSetup={SourceConfigSetup}
       />
     </div>
   );
--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -3,12 +3,20 @@ import AssetMenuOptions from "../assets/Menu";
 
 // components
 import Menu from "../components/menu/Menu";
+import Source from "../components/source/Source";
 
 const Settings = function () {
   return (
     <div className="container">
       <Menu options={AssetMenuOptions} />
-      <div className="main">Settings</div>
+      <div className="main">
+        <Source
+          initialSourceValue={`const settings = {
+  fontSize: 16,
+  tabSize: 4,
+};`}
+        />
+      </div>
     </div>
   );
 };
