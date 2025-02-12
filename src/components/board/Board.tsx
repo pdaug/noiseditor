b739544d8b3aa4dfd8f658343fb764daae711114
pdaug <dev.pdaug@gmail.com>
2025-02-12 11:03:03 -0300

    add: elements in board

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,23 +1,46 @@
-import { ReactFlow, Background, Controls } from "@xyflow/react";
+import { useCallback, useState } from "react";
+import {
+  ReactFlow,
+  Background,
+  Node,
+  applyNodeChanges,
+  addEdge,
+  NodeChange,
+  applyEdgeChanges,
+  Edge,
+  EdgeChange,
+  Connection,
+} from "@xyflow/react";
 
 // styles
 import "./Board.css";
 import "@xyflow/react/dist/style.css";
 
-const nodes = [
+// elements
+import { BoardSquare, BoardText } from "./BoardElement";
+
+const nodeTypes = {
+  BoardSquare,
+  BoardText,
+};
+
+const initialNodes: Node[] = [
   {
     id: "1",
     position: { x: 0, y: 0 },
-    data: { label: "Hello" },
+    data: {},
+    type: "BoardSquare",
+    dragHandle: ".board-top",
   },
   {
     id: "2",
-    data: { label: "World" },
-    position: { x: 100, y: 100 },
+    position: { x: 400, y: 0 },
+    data: {},
+    type: "BoardText",
   },
 ];
 
-const edges = [
+const initialEdges = [
   {
     id: "1-2",
     source: "1",
@@ -26,11 +49,55 @@ const edges = [
 ];
 
 const Board = function () {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    function (updateNode: NodeChange<Node>[]) {
+      const cloneNodes = window.structuredClone([...nodes]);
+      const newNodes = applyNodeChanges(updateNode, cloneNodes);
+      setNodes(newNodes);
+      return;
+    },
+    [nodes, setNodes],
+  );
+
+  const onEdgesChange = useCallback(
+    function (updateEdge: EdgeChange<Edge>[]) {
+      const cloneEdges = window.structuredClone([...edges]);
+      const newEdge = applyEdgeChanges(updateEdge, cloneEdges);
+      setEdges(newEdge);
+      return;
+    },
+    [edges, setEdges],
+  );
+
+  const onConnect = useCallback(
+    function (updateEdge: Connection) {
+      const cloneEdges = window.structuredClone([...edges]);
+      const newEdge = addEdge(updateEdge, cloneEdges);
+      setEdges(newEdge);
+      return;
+    },
+    [edges, setEdges],
+  );
+
   return (
     <div className="board">
-      <ReactFlow nodes={nodes} edges={edges}>
-        <Background />
-        <Controls />
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        nodeTypes={nodeTypes}
+        onConnect={onConnect}
+        onEdgesChange={onEdgesChange}
+        onNodesChange={onNodesChange}
+        proOptions={{ hideAttribution: true }}
+        onInit={function (instance) {
+          instance.fitView();
+          instance.zoomTo(1);
+          return;
+        }}>
+        <Background bgColor="#1f1f1f" color="#ff5d73" />
       </ReactFlow>
     </div>
   );
